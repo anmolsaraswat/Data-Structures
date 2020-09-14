5cdecbec234ab4201005656fe751c710468a5089
Anmol Saraswat <anmolsaraswat@outlook.com>
2020-09-14 22:48:42 +0530

    reversing a linked list

---
+++ Linked List/Reversing_LL.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+void reversingLinkedList(struct Node **head)
+{
+
+// Before changing next of current,
+// store next node
+//next = curr->next
+// Now change next of current
+// This is where actual reversing happens
+//curr->next = prev
+// Move prev and curr one step forward
+//prev = curr
+//curr = next
+
+    struct Node *previous, *current, *next;
+    previous = NULL;
+    current = *head;
+    next = NULL;
+    while(current != NULL)
+    {
+        next = current->next;
+        current->next = previous;
+        previous = current;
+        current = next;
+    }
+    *head = previous;
+}
+void printLinkedList(struct Node *head)
+{
+   struct Node *traverse;
+    traverse = head;
+    while (traverse != NULL)
+    {
+        if(traverse->next == NULL)
+        {
+            printf("%d", traverse->data);
+        }
+        else
+        {
+            printf("%d -->", traverse->data);
+        }
+        traverse = traverse->next;
+    }
+
+}
+
+int main(){
+
+    struct Node *head = NULL;
+    struct Node *str = NULL;
+    struct Node *ptr = NULL;
+    int no_of_ele;
+
+    printf("No of elements in a linked list: ");
+    scanf("%d", &no_of_ele);
+    head  = (struct Node*)malloc(sizeof(struct Node));
+    scanf("%d", &(head->data));
+    head->next = NULL;
+    for(int i=0; i<(no_of_ele-1); i++)
+    {
+        if(i==0)
+        {
+            ptr = (struct Node*)malloc(sizeof(struct Node));
+            scanf("%d", &(ptr->data));
+            head->next = ptr;
+            ptr->next = NULL;
+        }
+        else
+        {
+            str = (struct Node*)malloc(sizeof(struct Node));
+            scanf("%d", &(str->data));
+            str->next = NULL;
+            ptr->next = str;
+            ptr = str;
+        }
+    }
+    reversingLinkedList(&head);
+    printLinkedList(head);
+        return 0;
+}
