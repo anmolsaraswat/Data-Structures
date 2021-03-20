63bff1f239b3bfbe2e2c4bcf4d7bcac338e315d2
Anmol Saraswat <anmolsaraswat@outlook.com>
2021-03-20 19:02:48 +0530

    prefix to infix

---
+++ Stacks/PrefixtoInfix.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+prefixtoInfix(string prefix)
+{
+    std::stack<string> st;
+    string tempa, tempb, res;
+    for(int i=(prefix.size()-1); i>=0; i--)
+    {
+        if((prefix[i]>='a' && prefix[i]<='z') || (prefix[i]>='A' && prefix[i]<='Z'))
+            st.push(string(1,prefix[i]));
+        else
+        {
+            tempa = st.top(); st.pop();
+            tempb = st.top(); st.pop();
+            res = "(" + tempa + prefix[i] +tempb + ")";
+            st.push(res);
+        }
+    }
+    cout << st.top();
+}
+int main()
+{
+    string prefix = "*-A/BC-/AKL";
+    prefixtoInfix(prefix);
+    return 0;
+}
