6b84e8a88cebbc826d78fc078d03f2af63e52292
Anmol Saraswat <anmolsaraswat@outlook.com>
2021-03-21 22:58:05 +0530

    next greater element using stack.

--- Stacks/NextGreaterElementStack.cpp
+++ Stacks/NextGreaterElementStack.cpp
@@ -6,22 +6,22 @@ int main()
     std::stack<int> s;
     int arr[] = {11, 13, 21, 3, 11};
     int arrLen = sizeof(arr)/sizeof(arr[0]);
-    for(int i=0; i<arrLen; i++)
+    s.push(arr[0]);
+    for(int i=1; i<arrLen; i++)
     {
         if(s.empty())
         {
             s.push(arr[i]);
         }
-        else if(s.top() > arr[i])
-            s.push(arr[i]);
-        else
+        while(!s.empty() && (arr[i] > s.top()))
         {
-            while((arr[i] > s.top()) && !s.empty())
-            {
-                cout << "Next Greatest of " << s.top() << " is " << arr[i] << endl;
-                s.pop();
-            }
-            s.push(arr[i]);
+            cout << "Next Greatest of " << s.top() << " is " << arr[i] << endl;
+            s.pop();
         }
+        s.push(arr[i]);
+    }
+    while(!s.empty())
+    {
+        cout << s.top() << endl; s.pop();
     }
 }
