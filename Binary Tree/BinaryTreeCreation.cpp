a0becbc0a1b1e83a9c354b5e340e8cd4943494bf
Anmol Saraswat <anmolsaraswat@outlook.com>
2021-03-29 22:01:45 +0530

    Basics: Binary Tree Creation

---
+++ Binary Tree/BinaryTreeCreation.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* right;
+    struct Node* left;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int main()
+{
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    return 0;
+}
