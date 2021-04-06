54298e73ee56a95ea08ad673fc6b58c2f3613d83
Anmol Saraswat <anmolsaraswat@outlook.com>
2021-04-06 07:43:21 +0530

    Sum of all the heights of BT

--- Binary Tree/SumofAlltheHeight.cpp
+++ Binary Tree/SumofAlltheHeight.cpp
@@ -11,18 +11,24 @@ struct Node {
     }
 };
 
-int sumOfHeight(struct Node *root)
+int sumOfHeightUtil(struct Node *root)
 {
     if(!root)
-        return 1;
-    int lsum = sumOfHeight(root->left);
-    int rsum = sumOfHeight(root->right);
+        return 0;
+    int lsum = sumOfHeightUtil(root->left);
+    int rsum = sumOfHeightUtil(root->right);
     if (lsum > rsum)
         return (lsum + 1);
     else
         return (rsum + 1);
 }
 
+int sumOfHeight(struct Node *root)
+{
+    if (!root)
+        return 0;
+    return (sumOfHeightUtil(root) + sumOfHeight(root->left) + sumOfHeight(root->right));
+}
 int main()
 {
     struct Node* root = new Node(1);
