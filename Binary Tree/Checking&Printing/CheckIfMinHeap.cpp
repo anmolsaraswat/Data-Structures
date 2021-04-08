147e37ef18a2f887f23b7cf5855ca4cbaaeddc92
Anmol Saraswat <anmolsaraswat@outlook.com>
2021-04-09 00:23:13 +0530

    Check if the given level order traversal is a min heap

---
+++ Binary Tree/Checking&Printing/CheckIfMinHeap.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+bool isMinHeap(int level[], int n)
+{
+    for(int i=(n/2-1); i>=0; i--)
+    {
+        if(level[i] > level[2*i+1])
+            return false;
+
+        if(2*i+2 < n)
+        {
+            if(level[i] > level[2*i+2])
+                return false;
+        }
+    }
+}
+
+int main()
+{
+    int level[] = {10, 15, 14, 25, 30};
+    int n = sizeof(level)/sizeof(level[0]);
+    if(isMinHeap(level, n))
+        cout << "True";
+    else
+        cout << "False";
+
+    return 0;
+}
